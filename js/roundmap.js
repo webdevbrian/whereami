6621996464d48825b43837f189b5b38bd84842c9
Brian Kinney <webdevbrian@gmail.com>
2013-04-24 15:26:43 -0400

    End of round map (two point plotted map)

---
+++ js/roundmap.js
@@ -0,0 +1,37 @@
+      //
+      // End of round map
+      //
+
+      function rminitialize() {
+
+        var currentLLArr = locLatLongs.replace(/[\])}[{(]/g,'').split(',');
+        var GuessLLArr = guessLatLongs.replace(/[\])}[{(]/g,'').replace(/\s/g, "").split(',');
+        var actualLtLng = new google.maps.LatLng(currentLLArr[0],currentLLArr[1]);
+        var guessLtLng = new google.maps.LatLng(GuessLLArr[0],GuessLLArr[1]);
+
+        var mapOptions = {
+          zoom: 2,
+          center: actualLtLng,
+          mapTypeControl: false,
+          streetViewControl: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(document.getElementById("roundMap"), mapOptions);
+
+        var actualMarker = new google.maps.Marker({
+            position: actualLtLng,
+            title:"Actual Location",
+            icon: 'img/actual.png'
+        });
+
+        var guessMarker = new google.maps.Marker({
+            position: guessLtLng,
+            title:"Your Guess",
+            icon: 'img/guess.png'
+        });
+
+        // To add the marker to the map, call setMap();
+        actualMarker.setMap(map);
+        guessMarker.setMap(map);
+
+      };
